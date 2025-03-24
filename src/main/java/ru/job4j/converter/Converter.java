03337ad607f0d0583c9570bd0c67d99fcad51e52
Анатолий <eao73@mail.ru>
2025-03-24 21:33:03 +0300

    1.1. Что такое import [#505278]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -2,13 +2,23 @@ package ru.job4j.converter;
 
 public class Converter {
 
+    /**
+     *  Assuming the conversion rate is 1 Euro = 90 Rubles
+     * @param value
+     * @return
+     */
     public static double rubleToEuro(double value) {
-        //** Assuming the conversion rate is 1 Euro = 90 Rubles */
+
         return value / 90;
     }
 
+    /**
+     *   Assuming the conversion rate is 1 Dollar = 75 Rubles
+     * @param value
+     * @return
+     */
     public static double rubleToDollar(double value) {
-        //** Assuming the conversion rate is 1 Dollar = 75 Rubles */
+
         return value / 75;
     }
 }
