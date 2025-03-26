cfa07140fceab39159aebb2542509ce7a55b2a25
Анатолий <eao73@mail.ru>
2025-03-26 19:06:08 +0300

    1. Модульные тесты [#505277 #537201]

---
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -0,0 +1,24 @@
+package ru.job4j.converter;
+
+public class Converter {
+
+    /**
+     *  Assuming the conversion rate is 1 Euro = 90 Rubles
+     * @param value
+     * @return
+     */
+    public static double rubleToEuro(double value) {
+
+        return value / 90;
+    }
+
+    /**
+     *   Assuming the conversion rate is 1 Dollar = 75 Rubles
+     * @param value
+     * @return
+     */
+    public static double rubleToDollar(double value) {
+
+        return value / 75;
+    }
+}
