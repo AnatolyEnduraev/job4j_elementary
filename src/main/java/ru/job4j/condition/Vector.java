94b86dea458f39b8fea2330e5a4ee8e46e3bd76d
Анатолий <eao73@mail.ru>
2025-03-30 15:18:59 +0300

    Тестирование Класса Сложения Векторов [#505262 #537988]

--- src/main/java/ru/job4j/condition/Vector.java
+++ src/main/java/ru/job4j/condition/Vector.java
@@ -1,30 +1,13 @@
 package ru.job4j.condition;
 
-/**
- * Класс для работы с векторами.
- */
 public class Vector {
 
-    /**
-     * Метод выполняет сложение двух векторов.
-     *
-     * @param x1 координата x первого вектора.
-     * @param y1 координата y первого вектора.
-     * @param x2 координата x второго вектора.
-     * @param y2 координата y второго вектора.
-     * @return строка с координатами результирующего вектора.
-     */
     public static String addVectors(int x1, int y1, int x2, int y2) {
         int xResult = x1 + x2;
         int yResult = y1 + y2;
         return String.format("(%d, %d)", xResult, yResult);
     }
 
-    /**
-     * Точка входа в программу.
-     *
-     * @param args аргументы командной строки.
-     */
     public static void main(String[] args) {
         String result = addVectors(1, 2, 3, 4);
         System.out.println("result (1, 2) + (3, 4) = " + result);
