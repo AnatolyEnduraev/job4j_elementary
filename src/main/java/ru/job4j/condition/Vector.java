dd750a8f167bb650a674ca81a57045fe7037b185
Анатолий <eao73@mail.ru>
2025-03-30 15:54:06 +0300

    Тестирование Класса Расстояния между Точками [#505280 #537992]

--- src/main/java/ru/job4j/condition/Vector.java
+++ src/main/java/ru/job4j/condition/Vector.java
@@ -1,16 +1,6 @@
 package ru.job4j.condition;
 
 public class Vector {
-
-    /**
-     * Метод выполняет сложение двух векторов.
-     *
-     * @param x1 координата x первого вектора.
-     * @param y1 координата y первого вектора.
-     * @param x2 координата x второго вектора.
-     * @param y2 координата y второго вектора.
-     * @return строка с координатами результирующего вектора.
-     */
     public static String addVectors(int x1, int y1, int x2, int y2) {
         int xResult = x1 + x2;
         int yResult = y1 + y2;
@@ -22,6 +12,7 @@ public class Vector {
         System.out.println("result (1, 2) + (3, 4) = " + result);
     }
 }
+
 
 
 
