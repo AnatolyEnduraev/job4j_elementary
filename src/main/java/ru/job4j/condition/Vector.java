3f1d592ae1e19588c91c3e011ae047b5f5da3083
Анатолий <eao73@mail.ru>
2025-03-30 15:03:57 +0300

    Тестирование Класса Сложения Векторов [#505262 #537988]

--- src/main/java/ru/job4j/condition/Vector.java
+++ src/main/java/ru/job4j/condition/Vector.java
@@ -1,8 +1,17 @@
 package ru.job4j.condition;
 
 public class Vector {
+
+    /**
+     * Метод выполняет сложение двух векторов.
+     *
+     * @param x1 координата x первого вектора.
+     * @param y1 координата y первого вектора.
+     * @param x2 координата x второго вектора.
+     * @param y2 координата y второго вектора.
+     * @return строка с координатами результирующего вектора.
+     */
     public static String addVectors(int x1, int y1, int x2, int y2) {
-        // Сложение векторов: (x1, y1) + (x2, y2) = (x1 + x2, y1 + y2)
         int xResult = x1 + x2;
         int yResult = y1 + y2;
         return String.format("(%d, %d)", xResult, yResult);
@@ -13,6 +22,7 @@ public class Vector {
         System.out.println("result (1, 2) + (3, 4) = " + result);
     }
 }
+
 
 
 
--- src/test/java/ru/job4j/condition/VectorTest.java
+++ src/test/java/ru/job4j/condition/VectorTest.java
@@ -29,3 +29,4 @@ class VectorTest {
         assertThat(output).isEqualTo(expected);
     }
 }
+
