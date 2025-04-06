6708cbc1e6540b03f0bfebf4ab1f09948c4d6eb9
Анатолий <eao73@mail.ru>
2025-04-06 19:02:35 +0300

    Перевернуть массив [#505231]

---
+++ src/main/java/ru/job4j/array/Turn.java
@@ -0,0 +1,16 @@
+package ru.job4j.array;
+
+public class Turn {
+    public static int[] back(int[] array) {
+        int length = array.length;
+        for (int index = 0; index < length / 2; index++) {
+            int temp = array[index];
+            array[index] = array[length - 1 - index];
+            array[length - 1 - index] = temp;
+        }
+        return array;
+    }
+}
+
+
+
