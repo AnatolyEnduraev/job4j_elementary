f27889cd416c98ae03318754583ad7722868e297
Анатолий <eao73@mail.ru>
2025-04-07 17:33:16 +0300

    Слово начинается с ... [#505233 #538871]

--- src/main/java/ru/job4j/array/ArrayChar.java
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -2,9 +2,6 @@ package ru.job4j.array;
 
 public class ArrayChar {
     public static boolean startsWith(char[] word, char[] prefix) {
-        if (prefix.length > word.length) {
-            return false;
-        }
             for (int index = 0; index < prefix.length; index++) {
             if (word[index] != prefix[index]) {
                 return false;
