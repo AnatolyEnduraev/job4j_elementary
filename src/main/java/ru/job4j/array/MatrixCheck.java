bf75183df0971c005ba519e3bff7f96319662e31
Анатолий <eao73@mail.ru>
2025-04-09 21:35:49 +0300

    6.7.3. Массив из диагонали матрицы. [#505247]

--- src/main/java/ru/job4j/array/MatrixCheck.java
+++ src/main/java/ru/job4j/array/MatrixCheck.java
@@ -3,7 +3,7 @@ package ru.job4j.array;
 public class MatrixCheck {
     public static boolean monoHorizontal(char[][] board, int row) {
         boolean result = true;
-        for (int index = 1; index < board[row].length; index++) {
+        for (int index = 0; index < board[row].length; index++) {
             if (board[row][index] != board[row][0]) {
                 result = false;
                 break;
@@ -13,7 +13,7 @@ public class MatrixCheck {
     }
 
     public static boolean monoVertical(char[][] board, int column) {
-        for (int row = 1; row < board.length; row++) {
+        for (int row = 0; row < board.length; row++) {
             if (board[row][column] != board[0][column]) {
                 return false;
             }
@@ -28,5 +28,17 @@ public class MatrixCheck {
         }
         return result;
     }
+
+    public static boolean isWin(char[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][i] == 'X') {
+                if (monoHorizontal(board, i) || monoVertical(board, i)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
+
 
