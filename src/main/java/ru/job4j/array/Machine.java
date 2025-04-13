e6ff7747a7f74199e0c7e237acd692fb02014640
Анатолий <eao73@mail.ru>
2025-04-13 12:09:15 +0300

    6.8. Метод двух указателей

--- src/main/java/ru/job4j/array/Machine.java
+++ src/main/java/ru/job4j/array/Machine.java
@@ -5,8 +5,16 @@ import java.util.Arrays;
 public class Machine {
     public static int[] change(int money, int price) {
         int[] coins = {10, 5, 2, 1};
+        int change = money - price;
         int[] result = new int[100];
         int size = 0;
+
+        for (int coin : coins) {
+            while (change >= coin) {
+                result[size++] = coin;
+                change -= coin;
+            }
+        }
         return Arrays.copyOf(result, size);
     }
 }
