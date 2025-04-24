0a24dfbf4c89ddb892db5668c7c43ad997bae7e3
Анатолий <eao73@mail.ru>
2025-04-24 20:35:43 +0300

    2.0. Рефакторинг - Расстояние между точками. [#122587]

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -1,14 +1,38 @@
 package ru.job4j.condition;
 
+import static java.lang.Math.sqrt;
+import static java.lang.Math.pow;
+
+/**
+ * Класс Point представляет точку на двумерной плоскости.
+ */
 public class Point {
-    public static double distance(int x1, int y1, int x2, int y2) {
-        return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+    private int x;
+    private int y;
+
+    /**
+     * Конструктор для инициализации точки с заданными координатами.
+     * @param first координата X
+     * @param second координата Y
+     */
+    public Point(int first, int second) {
+        this.x = first;
+        this.y = second;
+    }
+
+    /**
+     * Метод для вычисления расстояния до другой точки.
+     * @param that другая точка
+     * @return расстояние между текущей точкой и заданной точкой
+     */
+    public double distance(Point that) {
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
     }
 
     public static void main(String[] args) {
-        double result = distance(0, 0, 2, 0);
-        System.out.println("result (0, 0) to (2, 0) " + result);
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        double distance = a.distance(b);
+        System.out.println("Расстояние между точками: " + distance);
     }
 }
-
-
